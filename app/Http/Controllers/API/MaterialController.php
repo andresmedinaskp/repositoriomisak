4daaa19f39568e6e60aceb105baf17fea03dd95c
03Kristian <cristian.jacanamijoy@misena.edu.co>
2022-04-23 13:48:07 -0500

    se modifico MaterialController y UserController

--- app/Http/Controllers/API/MaterialController.php
+++ app/Http/Controllers/API/MaterialController.php
@@ -27,6 +27,11 @@ class MaterialController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
+    /* public function download($pdf){
+        $material=Material::where('pdf',$pdf)->findOrFail();
+        $pathToFile=storage_path("app/public/file/");
+    } */
+
     public function store(Request $request)
     {
         $validar= Validator::make($request->all(), [
@@ -36,35 +41,46 @@ class MaterialController extends Controller
             'num_pages' => 'required',
             'priority' => 'required',
             'pdf' => 'required',
-            'img' => 'required',
-            'type_material_id' => 'required', 
-            'editorial_id' => 'required',
-            'area_id' => 'required'
-        ]); 
-        if(!$validar ->fails()){
+            'img' => 'required|max:1024'
+        ]);
+
+
+        /* if(!$validar ->fails()){ */
             $material = new Material();
-            
+
+            $image="";
+                if($request->hasFile('img')){
+            $image=$request->file('img')->store('image','public');
+                }else{
+                $image=Null;
+                }
+                $file="";
+                if($request->hasFile('pdf')){
+            $file=$request->file('pdf')->store('file','public');
+                }else{
+                $file=Null;
+                }
             $material->name = $request ->name;
             $material->isbn = $request ->isbn;
             $material->year = $request ->year;
             $material->num_pages = $request ->num_pages;
             $material->priority = $request ->priority;
-            $material->pdf = $request ->pdf;
-            $material->img = $request ->img;
+            $material->pdf = $file;
+            $material->img = $image;
             $material->type_material_id = $request ->type_material_id;
             $material->editorial_id = $request ->editorial_id;
             $material->area_id = $request ->area_id;
 
-            $material->save();
-
+            $result=$material->save();
+        if($result){
             return response()->json([
                 'res'=> true,
-                'mensaje' => 'material guardado' 
+                'mensaje' => 'material guardado'
             ]);
-        }else{
+            }else{
             return response()->json([
                 'res'=> false,
-                'mensaje' => 'error entrada duplicada' 
+                'mensaje' => 'error entrada duplicada'
             ]);
         }
     }
@@ -82,12 +98,12 @@ class MaterialController extends Controller
         if (isset($material)){
             return response()->json([
                 'res'=> true,
-                'material' => $material 
+                'material' => $material
             ]);
         }else{
             return response()->json([
                 'res'=> false,
-                'mensaje' => 'registro no encontrado' 
+                'mensaje' => 'registro no encontrado'
             ]);
         }
     }
@@ -109,7 +125,7 @@ class MaterialController extends Controller
             'priority' => 'required',
             'pdf' => 'required',
             'img' => 'required',
-            'type_material_id' => 'required', 
+            'type_material_id' => 'required',
             'editorial_id' => 'required',
             'area_id' => 'required'
         ]);
@@ -131,7 +147,7 @@ class MaterialController extends Controller
                 $material->save();
                  return response()->json([
                 'res'=> true,
-                'mensaje' => 'material actualizado' 
+                'mensaje' => 'material actualizado'
             ]);
 
             }else{
--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -7,6 +7,7 @@ use Illuminate\Validation\Rules\Exist;
 use Illuminate\Support\Facades\Validator;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
@@ -159,5 +160,82 @@ class UserController extends Controller
                 'mensaje' => 'falla al elimar no se encontro registro'
             ]);
         }
+    }
+    public function register(Request $request) {
+        $request->validate([
+            'name' => 'required',
+            'full_name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|confirmed'            
+        ]);
+
+        $user = new User();
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->full_name = $request->full_name;
+        $user->document_type = $request->document_type;
+        $user->document_number = $request->document_number;
+        $user->certificate_misak = $request->certificate_misak;
+        $user->password = Hash::make($request->password);
+        $user->save();
+        
+
+        return response()->json([
+            "status" => 1,
+            "msg" => "¡Registro de usuario exitoso!",
+        ]);    
+    }
+
+
+    public function login(Request $request) {
+
+        $request->validate([
+            "email" => "required|email",
+            "password" => "required"
+        ]);
+
+        $user = User::where("email", "=", $request->email)->first();
+        // revisamos si el id es existente
+        if( isset($user->id) ){
+            // revisamos la encriptacion
+            if(Hash::check($request->password, $user->password)){
+                //creamos el token
+                $token = $user->createToken("auth_token")->plainTextToken;
+                //si está todo es correcto
+                return response()->json([
+                    "status" => 1,
+                    "msg" => "usuario correctamente logeado",
+                    "access_token" => $token
+                ]);        
+            }else{
+                return response()->json([
+                    "status" => 0,
+                    "msg" => "password incorrecto",
+                ]);    
+            }
+
+        }else{
+            return response()->json([
+                "status" => 0,
+                "msg" => "Usuario no registrado",
+            ]);  
+        }
+    }
+
+    public function userProfile() {
+        return response()->json([
+            "status" => 0,
+            "msg" => "Acerca del perfil de usuario",
+            "data" => auth()->user()
+        ]); 
+    }
+
+    public function logout() {
+        auth()->user()->tokens()->delete();
+        
+        return response()->json([
+            "status" => 1,
+            "msg" => "Cierre de Sesión",            
+        ]); 
     }
 }
